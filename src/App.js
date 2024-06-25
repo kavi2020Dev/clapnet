672ae9e928de81323d0ae76d10daf88795cdb8ff
kavibluekode <kavi@bluekode.com>
2024-06-25 18:34:01 +0530

    customize 4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div style={{height:'100vh', width:'100vw', backgroundColor:'black', color:'white'}}>Calpnet</div>
+    <div style={{height:'100vh', width:'100vw', backgroundColor:'black', alignItems:'center', color:'white', display:'flex', justifyContent:'center'}}>
+      <h2>Calpnet</h2>
+    </div>
   )
 }
 
